840bf1d289dde3414fc4a03696ab1bc3c109a313
Stephen Brennan <smb196@case.edu>
2015-06-10 22:27:23 -0400

    Added location and block structs.  Coded constants for all tetromino orientations.

--- src/tetris.c
+++ src/tetris.c
@@ -13,21 +13,106 @@
 
 *******************************************************************************/
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include "tetris.h"
+
+tetris_location TETROMINOS[NUM_TETROMINOS][NUM_ORIENTATIONS][TETRIS] = {
+  // I
+  {
+    {{1, 0},  {1, 1},
+      {1, 2},  {1, 3}},
+    { {0, 2},  {1, 2},
+      {2, 2},  {3, 2}},
+    { {3, 0},  {3, 1},
+      {3, 2},  {3, 3}},
+    { {0, 1},  {1, 1},
+      {2, 1},  {3, 1}}
+  },
+  // J
+  {
+    { {0, 0},  {1, 0},
+      {1, 1},  {1, 2}},
+    { {0, 1},  {0, 2},
+      {1, 1},  {2, 1}},
+    { {1, 0},  {1, 1},
+      {1, 2},  {2, 2}},
+    { {0, 1},  {1, 1},
+      {2, 0},  {2, 1}}
+  },
+  // L
+  {
+    { {0, 2},  {1, 0},
+      {1, 1},  {1, 2}},
+    { {0, 1},  {1, 1},
+      {2, 1},  {2, 2}},
+    { {1, 0},  {1, 1},
+      {1, 2},  {2, 0}},
+    { {0, 0},  {0, 1},
+      {1, 1},  {2, 1}}
+  },
+  // O
+  {
+    { {0, 1},  {0, 2},
+      {1, 1},  {1, 2}},
+    { {0, 1},  {0, 2},
+      {1, 1},  {1, 2}},
+    { {0, 1},  {0, 2},
+      {1, 1},  {1, 2}},
+    { {0, 1},  {0, 2},
+      {1, 1},  {1, 2}}
+  },
+  // S
+  {
+    { {0, 1},  {0, 2},
+      {1, 0},  {1, 1}},
+    { {0, 1},  {1, 1},
+      {1, 2},  {2, 2}},
+    { {1, 1},  {1, 2},
+      {2, 0},  {2, 1}},
+    { {0, 0},  {1, 0},
+      {1, 1},  {2, 1}}
+  },
+  // T
+  {
+    { {0, 1},  {1, 0},
+      {1, 1},  {1, 2}},
+    { {0, 1},  {1, 1},
+      {1, 2},  {2, 1}},
+    { {1, 0},  {1, 1},
+      {1, 2},  {2, 1}},
+    { {0, 1},  {1, 0},
+      {1, 1},  {2, 1}}
+  },
+  // Z
+  {
+    { {0, 0},  {0, 1},
+      {1, 1},  {1, 2}},
+    { {0, 2},  {1, 1},
+      {1, 2},  {2, 1}},
+    { {1, 0},  {1, 1},
+      {2, 1},  {2, 2}},
+    { {0, 1},  {1, 0},
+      {1, 1},  {2, 0}}
+  },
+};
 
 void tg_init(tetris_game *obj, int rows, int cols)
 {
   // Initialization logic
   obj->rows = rows;
   obj->cols = cols;
-  obj->board = smb_new(char, rows * cols);
+  obj->board = malloc(rows * cols);
+  memset(obj->board, TG_EMPTY, rows * cols);
   obj->points = 0;
   obj->level = 1;
 }
 
 tetris_game *tg_create(int rows, int cols)
 {
-  tetris_game *obj = smb_new(tetris_game, 1);
+  tetris_game *obj = malloc(sizeof(tetris_game));
   tg_init(obj, rows, cols);
   return obj;
 }
@@ -35,11 +120,52 @@ tetris_game *tg_create(int rows, int cols)
 void tg_destroy(tetris_game *obj)
 {
   // Cleanup logic
-  smb_free(obj->board);
+  free(obj->board);
 }
 
 void tg_delete(tetris_game *obj) {
   tg_destroy(obj);
-  smb_free(obj);
+  free(obj);
 }
 
+char tg_get(tetris_game *obj, int row, int column)
+{
+  return obj->board[obj->cols * row + column];
+}
+
+void tg_set(tetris_game *obj, int row, int column, char value)
+{
+  obj->board[obj->cols * row + column] = value;
+}
+
+void tg_put(tetris_game *obj, tetris_block block)
+{
+  int i;
+  for (i = 0; i < TETRIS; i++) {
+    tetris_location cell = TETROMINOS[block.typ][block.ori][i];
+    tg_set(obj, block.loc.row + cell.row, block.loc.col + cell.col, TG_BLOCK);
+  }
+}
+
+void tg_remove(tetris_game *obj, tetris_block block)
+{
+  int i;
+  for (i = 0; i < TETRIS; i++) {
+    tetris_location cell = TETROMINOS[block.typ][block.ori][i];
+    tg_set(obj, block.loc.row + cell.row, block.loc.col + cell.col, TG_EMPTY);
+  }
+}
+
+void tg_print(tetris_game *obj, FILE *f) {
+  int i, j;
+  for (i = 0; i < obj->rows; i++) {
+    for (j = 0; j < obj->cols; j++) {
+      if (tg_get(obj, i, j) == TG_EMPTY) {
+        fputs(TG_EMPTY_STR, f);
+      } else {
+        fputs(TG_BLOCK_STR, f);
+      }
+    }
+    fputc('\n', f);
+  }
+}
--- src/tetris.h
+++ src/tetris.h
@@ -16,6 +16,40 @@
 #ifndef TETRIS_H
 #define TETRIS_H
 
+#include <stdio.h> // for FILE
+
+#define TETRIS 4
+
+#define TG_EMPTY   0x00
+#define TG_FALLING 0x01
+#define TG_BLOCK   0x02
+
+#define TET_I 0
+#define TET_J 1
+#define TET_L 2
+#define TET_O 3
+#define TET_S 4
+#define TET_T 5
+#define TET_Z 6
+#define NUM_TETROMINOS 7
+#define NUM_ORIENTATIONS 4
+
+#define TG_EMPTY_STR " "
+#define TG_BLOCK_STR "\u25AE"
+
+typedef struct {
+  int row;
+  int col;
+} tetris_location;
+
+typedef struct {
+
+  int typ;
+  int ori;
+  tetris_location loc;
+
+} tetris_block;
+
 typedef struct {
 
   int rows;
@@ -26,9 +60,15 @@ typedef struct {
 
 } tetris_game;
 
+extern tetris_location TETROMINOS[NUM_TETROMINOS][NUM_ORIENTATIONS][TETRIS];
+
 void tg_init(tetris_game *obj, int rows, int cols);
 tetris_game *tg_create(int rows, int cols);
 void tg_destroy(tetris_game *obj);
 void tg_delete(tetris_game *obj);
+
+char tg_get(tetris_game *obj, int row, int col);
+void tg_set(tetris_game *obj, int row, int col, char value);
+void tg_print(tetris_game *obj, FILE *f);
 
 #endif // TETRIS_H

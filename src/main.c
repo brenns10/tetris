3d1b9a528221cfb1aa04fcc85b3195fc85aa7164
Stephen Brennan <smb196@case.edu>
2015-06-10 22:41:07 -0400

    Updated to simple driver program that demos all the tetrominos and orientations.

--- src/main.c
+++ src/main.c
@@ -20,8 +20,16 @@
 int main(int argc, char *argv[])
 {
   tetris_game *tg = tg_create(10, 10);
-  tg_print(tg, stdout);
-  tg_set(tg, 5, 5, TG_BLOCK);
+  tetris_block tb = {0, 0, {1, 1}};
+  for (tb.typ = 0; tb.typ < NUM_TETROMINOS; tb.typ++) {
+    for (tb.ori = 0; tb.ori < NUM_ORIENTATIONS; tb.ori++) {
+      tg_put(tg, tb);
+      tg_print(tg, stdout);
+      getchar();
+      printf("-------------------------\n");
+      tg_remove(tg, tb);
+    }
+  }
   tg_print(tg, stdout);
   tg_delete(tg);
   return 0;

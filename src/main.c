376b76e04e477d8af25d513af50be9d401b2f019
Stephen Brennan <smb196@case.edu>
2015-06-11 16:50:57 -0400

    Added boss mode.

--- src/main.c
+++ src/main.c
@@ -22,6 +22,32 @@
 #include "tetris.h"
 #include "util.h"
 
+void boss_mode(void)
+{
+  clear();
+  printw("user@workstation-312:~/Documents/presentation $ ls -l\n"
+         "total 528\n"
+         "drwxr-xr-x 2 user users   4096 Jun  9 17:05 .\n"
+         "drwxr-xr-x 4 user users   4096 Jun 10 09:52 ..\n"
+         "-rw-r--r-- 1 user users  88583 Jun  9 14:13 figure1.png\n"
+         "-rw-r--r-- 1 user users  65357 Jun  9 15:40 figure2.png\n"
+         "-rw-r--r-- 1 user users   4469 Jun  9 16:17 presentation.aux\n"
+         "-rw-r--r-- 1 user users  42858 Jun  9 16:17 presentation.log\n"
+         "-rw-r--r-- 1 user users   2516 Jun  9 16:17 presentation.nav\n"
+         "-rw-r--r-- 1 user users    183 Jun  9 16:17 presentation.out\n"
+         "-rw-r--r-- 1 user users 349607 Jun  9 16:17 presentation.pdf\n"
+         "-rw-r--r-- 1 user users      0 Jun  9 16:17 presentation.snm\n"
+         "-rw-r--r-- 1 user users   9284 Jun  9 17:05 presentation.tex\n"
+         "-rw-r--r-- 1 user users    229 Jun  9 16:17 presentation.toc\n"
+         "\n"
+         "user@workstation-312:~/Documents/presentation $ ");
+  echo();
+  timeout(-1);
+  while (getch() != KEY_F(1));
+  timeout(0);
+
+}
+
 int main(int argc, char **argv)
 {
   tetris_game *tg = tg_create(22, 10);
@@ -67,6 +93,10 @@ int main(int argc, char **argv)
       timeout(-1);
       getch();
       timeout(0);
+      move = TM_NONE;
+      break;
+    case 'b':
+      boss_mode();
       move = TM_NONE;
       break;
     default:

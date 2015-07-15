46824caea85e036500004c94b45a25f72d7b6650
Stephen Brennan <smb196@case.edu>
2015-07-15 12:54:56 -0400

    Removed my bad sleep function and went with a Unix-only solution using nanosleep.

--- src/util.c
+++ src/util.c
@@ -13,24 +13,13 @@
 
 *******************************************************************************/
 
-#ifdef WIN32
-#include <windows.h> // Sleep
-#elif _POSIX_C_SOURCE >= 199309L
 #include <time.h>    // nanosleep
-#else
-#include <unistd.h>  // usleep
-#endif
+
 
 void sleep_milli(int milliseconds)
 {
-#ifdef WIN32
-  Sleep(milliseconds);
-#elif _POSIX_C_SOURCE >= 199309L
   struct timespec ts;
   ts.tv_sec = 0;
-  ts.tv_nsec = milleseconds * 1000 * 1000;
+  ts.tv_nsec = milliseconds * 1000 * 1000;
   nanosleep(&ts, NULL);
-#else
-  usleep(milliseconds * 1000);
-#endif
 }
